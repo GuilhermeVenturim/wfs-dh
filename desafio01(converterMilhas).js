20973c78b036c891138efc515f51b983f9519346
GuilhermeVenturim <91443441+GuilhermeVenturim@users.noreply.github.com>
2022-06-09 12:44:58 -0300

    Delete desafio01(converterMilhas).js

--- desafio01(converterMilhas).js
+++
@@ -1,9 +0,0 @@
-let quilometrosPercorridosPeloVeiculo = 800000;
-
-function converter(milhas){
-    return milhas = quilometrosPercorridosPeloVeiculo / 1.6
-}
-
-console.log(converter())
-// Utilize a mensagem a seguir como base do retorno esperado:
-// Seu veículo percorreu um total de XXXXXX milhas.
