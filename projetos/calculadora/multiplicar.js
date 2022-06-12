68a3f6b5251adb3edf1b39d0c3738525836c8d51
GuilhermeVenturim <guilhermeventurim@outlook.com>
2022-06-12 18:33:18 -0300

    Função multiplicar funcional. Teste com 0 retornando mensagem. UP

--- projetos/calculadora/multiplicar.js
+++ projetos/calculadora/multiplicar.js
@@ -1,4 +1,10 @@
-function somar(numA, numB){
-    let resultado = numA + numB
-    return resultado
+function multiplicar(numA, numB){
+    let resultado = []
+    if(numA != 0 && numB != 0){
+        return resultado = numA * numB;
+    }else{
+        return 'Por favor, tente novamente com um número diferente de 0'
+    }
 }
+//console.log(multiplicar(10, 0)) //testado
+//console.log(multiplicar(10, 2)) //testado
