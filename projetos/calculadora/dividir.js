31d2fbc12afbb0a6dcac03877f4ce30d125efea6
GuilhermeVenturim <guilhermeventurim@outlook.com>
2022-06-12 18:24:22 -0300

    Função dividir funcional. Testado com 0 como divisor. UP!

--- projetos/calculadora/dividir.js
+++ projetos/calculadora/dividir.js
@@ -1,4 +1,10 @@
-function somar(numA, numB){
-    let resultado = numA + numB
-    return resultado
+function dividir(numA, numB){
+    let resultado = []
+    if(numA != 0 && numB != 0){
+        return resultado = numA / numB;
+    }else{
+        return 'Por favor, tente novamente com um número diferente de 0'
+    }
 }
+//console.log(dividir(10, 0)) testado
+//console.log(dividir(10, 2)) testado
