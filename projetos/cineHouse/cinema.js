9b8dc26353963b4dbf21e9fab121a3af6f2bee97
GuilhermeVenturim <guilhermeventurim@outlook.com>
2022-06-13 21:28:09 -0300

    Atualização do código cineHouse. v1.01

--- projetos/cineHouse/cinema.js
+++ projetos/cineHouse/cinema.js
@@ -1,39 +1,7 @@
+let catalogo = require('./database/catalogo.json')
+
 let cinema = 'CineHouse'
 
-let catalogo =  [   {
-        codigo: 01,
-        nome: 'O senhor dos anéis: A sociedade do anel',
-        duracao: 3,
-        atores: 'Elijah Wood, Sean Astin, Ian McKellen',
-        lancamento: 2002,
-        emCartaz: false
-        
-    },
-    {
-        codigo : 02,
-        nome: 'O senhor dos anéis: As duas torres',
-        duracao: 3,
-        atores: 'Elijah Wood, Sean Astin, Viggo Mortensen',
-        lancamento: 2002,
-        emCartaz : false
-    },
-    {
-        codigo: 03,
-        nome: 'O senhor dos anéis: O retorno do rei',
-        duracao: 3.2,
-        atores: 'Elijah Wood, Sean Astin, Viggo Mortensen',
-        lancamento: 2003,
-        emCartaz: false
-    },
-    {
-        codigo: 04,
-        nome: 'Doutor Estranho no Multiverso da Loucura',
-        duracao: 2,
-        atores: 'Benedict Cumberbatch, Elizabeth Olsen, Chiwetel Ejiofor',
-        lancamento: 2022,
-        emCartaz: true
-    }
-]
 function AdicionarFilme(codigo, nome, duracao, atores, lancamento, emCartaz){
         this.codigo     = codigo
         this.nome       = nome
@@ -43,7 +11,7 @@ function AdicionarFilme(codigo, nome, duracao, atores, lancamento, emCartaz){
         this.emCartaz   = emCartaz
 }
 
-let newFilme = new AdicionarFilme(04, 'Doutor Estranho no Multiverso da Loucura', 2, 'Benedict Cumberbatch, Elizabeth Olsen, Chiwetel Ejiofor', 2022, true)
+let newFilme = new AdicionarFilme(4, 'Doutor Estranho no Multiverso da Loucura', 2, 'Benedict Cumberbatch, Elizabeth Olsen, Chiwetel Ejiofor', 2022, true)
 
 
 function buscarFilme(index){
@@ -51,7 +19,7 @@ function buscarFilme(index){
         return resultado
 }
 
-function alterarStatusEmCartaz(index, nomeFilme){
+function alterarStatusEmCartaz(index){
         //let statusEmCartazAlterado = []
         if(catalogo[index].emCartaz == false){
             return `O Filme ${catalogo[index].nome} está em cartaz?  ${catalogo[index].emCartaz = true}`
@@ -61,8 +29,8 @@ function alterarStatusEmCartaz(index, nomeFilme){
         //catalogo[index].emCartaz != true
 }
 
-//console.log(catalogo)
+console.log(catalogo.codigo[2])
 //console.log(newFilme)
 //console.log(catalogo, newFilme)
 //console.log(buscarFilme(2))
-console.log(alterarStatusEmCartaz(3))
+//console.log(alterarStatusEmCartaz(3))
