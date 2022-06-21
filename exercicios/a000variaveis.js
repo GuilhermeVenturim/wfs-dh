c7301e91846387cc36e069aae6e8ef2a6f52bfd0
GuilhermeVenturim <guilhermeventurim@outlook.com>
2022-06-20 23:46:27 -0300

    Exercício concluído

---
+++ exercicios/a000variaveis.js
@@ -0,0 +1,17 @@
+//Variáveis
+
+//Variável tipo String
+var meuNome = 'Guilherme Venturim';
+console.log(`Meu nome é ${meuNome}`);
+
+//Variável tipo Const
+const NASCIMENTO = 1991; //const geralmente é declarada no modo uppercase para facilitar o entendimento do código.
+console.log(`Nasci em ${NASCIMENTO}`);
+
+//Variável tipo Array
+let arrayFrutas = ['Maçã', 'Pêra', 'Uva', 'Morango', 'Melancia', 'Laranja', 'Limão', 'Banana', 'Mamão', 'Melão', 'Pêssego', 'Cupuaçú'];
+console.log(`Gosto das seguintes frutas: ${arrayFrutas}`);
+
+
+
+
