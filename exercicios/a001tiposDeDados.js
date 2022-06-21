ca349dd79eb33529ae5562d6416b747dacdef53c
GuilhermeVenturim <guilhermeventurim@outlook.com>
2022-06-21 11:21:16 -0300

    Exercício Tipos de dados concluído

---
+++ exercicios/a001tiposDeDados.js
@@ -0,0 +1,42 @@
+//Tipos de dados:
+
+//Numéricos.
+let anoNascimento = 1991;
+console.log(`Nascido em ${anoNascimento}`);
+
+//Cadeia de Caracteres - STRING.
+let nomeESobrenome = 'Guilherme Venturim';
+console.log(`Me chamo ${nomeESobrenome}`);
+
+//Lógicos ou Booleanos.
+let estudanteDeJavaScript = true;
+console.log(`Atualmente estudo programação Web com JavaScript? ${estudanteDeJavaScript}`);
+
+//Objeto
+let outrosDadosPessoais = {
+    solteiro: true,
+    altura: 1.80,
+    email: 'guilhermeventurim@outlook.com'
+};
+console.log(outrosDadosPessoais);
+
+//Array.
+let listaDeCompras = ['Café', 'Leite', 'Queijo', 'Manteiga', 'Suco de Uva'];
+console.log(listaDeCompras[0])
+
+//Tipos especiais de Dados:
+
+//NaN - Not A Number indica que o valor não pode ser passado como um número.
+let naoENumero = '1911' / 2;
+console.log(naoENumero);
+
+//Null = Valor nulo é atribuído para indicar um valor vazio ou desconhecido.
+let nomeDaEsposa = null;
+console.log(nomeDaEsposa);
+
+//Undefined - Valor indefinido é usado quando vamos passar valor em outro momento.
+let dataFinalCurso; //variável declarada sem valor.
+console.log(dataFinalCurso);
+dataFinalCurso = 'Novembro de 2022'; //atribuindo valor a váriavel.
+console.log(dataFinalCurso);
+
