aff842e288a66b86a494fc178094b6cca7d6583d
GuilhermeVenturim <guilhermeventurim@outlook.com>
2022-06-25 18:04:04 -0300

    Exercício finalizado

--- exercicios/a020express.js
+++ exercicios/a020express.js
@@ -1,2 +1,7 @@
+//Método para importar módulo express (framework node para criação de servidor local). Salvo em variavel tipo const.
+
 const express = require('express');
+
+//Váriavel que guarda as funcionalidades do express para ser usada posteriormente.
 const app = express();
+
