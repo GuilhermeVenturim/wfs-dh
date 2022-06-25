5b7bde7511b77018eb6eb8468db057c97a3b13ab
GuilhermeVenturim <guilhermeventurim@outlook.com>
2022-06-25 18:23:40 -0300

    020 att.

--- exercicios/a020express.js
+++ exercicios/a020express.js
@@ -5,3 +5,10 @@ const express = require('express');
 //Váriavel que guarda as funcionalidades do express para ser usada posteriormente.
 const app = express();
 
+app.get('/', (req, res)=>res.send('Olá mundo!'));//criado uma rota/spooler.
+
+//Criando servidor usando método listen, recebe o número da porta e um callback como parametros.
+app.listen(3000, ()=>console.log('Estamos usando a porta 3000!')); 
+
+
+
