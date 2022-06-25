e730f406551cde58398a8d3cf1427a7085ba82b2
GuilhermeVenturim <guilhermeventurim@outlook.com>
2022-06-25 17:35:59 -0300

    Adicionado módulo express. Concluídos exercícios módulo HTTP no node e node express.

---
+++ exercicios/a019app.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+http.createServer((req, res)=>{
+    res.writeHead(200, {'Content-type':'text/plain'});
+        switch (req.url) {
+            case '/':
+                res.end('Você está na página home!');
+
+            break;
+            case '/contato':
+                res.end('Você está na página contato!');
+                
+            break;
+            default:
+                res.end('Você está no nosso servidor!');
+                break;
+        }
+    res.end('Servidor online');
+}).listen(3000, "localhost");
+
+
+const express = require('express');
+const app = express();
